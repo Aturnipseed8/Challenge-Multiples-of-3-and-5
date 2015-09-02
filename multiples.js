f6be33e797c1eb2d7a792f292517e8c88484cceb
Anna <anna.turnipseed@gmail.com>
2015-09-01 19:22:48 -1000

    I really need to review my for loops

--- multiples.js
+++ multiples.js
@@ -3,10 +3,18 @@
  * @param  {Number} n Number that is to be used for calculating multiples up to that Number
  * @return {Number}
  */
+
+ n = 1000;
+
 exports.sumOfAMultiple = function( n ) {
-  var sum = 0;
-
-  // do your work here
-
+  var sum = 0 ;
+  var i = 0;
+ for(i = 0; i < n; i++) {
+ 	if(i % 3 === 0) {
+ 		sum = sum + i;
+ 	} else if (i % 5 === 0) {
+ 		sum = sum + i;
+	}
+ }
   return sum;
 };
